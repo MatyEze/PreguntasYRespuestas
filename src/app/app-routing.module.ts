5a4d4b0d241fd70a26ce4d2fa0128f758cb6b938
MatyEze <matiasezequielsellanes@hotmail.com>
2022-09-08 19:52:12 -0300

    refactorizado del routing de los componente de inicio por jerarquia de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/inicio/login/login.component';
 import { RegisterComponent } from './components/inicio/register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/bienvenida', pathMatch: 'full' },
-  { path: 'bienvenida', component: BienvenidaComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: '**', redirectTo: '/bienvenida', pathMatch: 'full' }
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent, children: [
+    { path: '', component: BienvenidaComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+  ]},
+
+  { path: '**', redirectTo: '/inicio', pathMatch: 'full' }
 ];
 
 @NgModule({
